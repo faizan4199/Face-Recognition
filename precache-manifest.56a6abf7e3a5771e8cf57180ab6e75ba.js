aa1565f9c6584f22be2301a356fe1fdec968508d
Faizan Shaikh <faizan4199@gmail.com>
2020-05-08 12:23:01 +0530

    Updates

---
+++ precache-manifest.56a6abf7e3a5771e8cf57180ab6e75ba.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c74bd1bf1574a1ccae062372ba84e993",
+    "url": "/Face-Recognition/index.html"
+  },
+  {
+    "revision": "11a579dbe4a68a00310e",
+    "url": "/Face-Recognition/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "4a7113a828803010044e",
+    "url": "/Face-Recognition/static/css/main.50029820.chunk.css"
+  },
+  {
+    "revision": "11a579dbe4a68a00310e",
+    "url": "/Face-Recognition/static/js/2.416722c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Face-Recognition/static/js/2.416722c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a7113a828803010044e",
+    "url": "/Face-Recognition/static/js/main.2f2e7984.chunk.js"
+  },
+  {
+    "revision": "3f551930e5ec4ad62872",
+    "url": "/Face-Recognition/static/js/runtime-main.eb13263c.js"
+  },
+  {
+    "revision": "83ba4c489e6256ac17ceab38049ccced",
+    "url": "/Face-Recognition/static/media/face.83ba4c48.png"
+  }
+]);
